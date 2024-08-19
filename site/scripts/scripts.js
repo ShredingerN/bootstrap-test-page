06ab84f71fb831194b8eddff58b84030fe5fcf77
NS Shneyder <nshneyder@ozon.ru>
2024-08-19 03:00:06 +0300

    bootstrap

---
+++ site/scripts/scripts.js
@@ -0,0 +1,29 @@
+// function toggleTheme() {
+//     const theme = document.documentElement.getAttribute('data-bs-theme');
+//     const btn = document.getElementById('theme-toggle-btn');
+
+//     if (theme === 'light') {
+//         document.documentElement.setAttribute('data-bs-theme', 'dark');
+//         btn.innerHTML = `🌙`;
+//     } else {
+//         document.documentElement.setAttribute('data-bs-theme', 'light');
+//         btn.innerHTML = `☀️`;
+//     }
+// }
+
+
+function toggleTheme() {
+    const theme = document.documentElement.getAttribute('data-bs-theme');
+    const btn = document.getElementById('theme-toggle-btn');
+
+    if (theme === 'light') {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        document.body.style.backgroundColor = 'var(--primary-color)';
+        btn.innerHTML = '🌖';
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+        document.body.style.backgroundColor = 'var(--primary-color)';
+        btn.innerHTML = '🌘';
+    }
+}
+
