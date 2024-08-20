e9f13560856acac376957853c62ba09877aecd2b
NS Shneyder <nshneyder@ozon.ru>
2024-08-21 01:00:55 +0300

    delete comments

--- site/scripts/scripts.js
+++ site/scripts/scripts.js
@@ -1,16 +1,3 @@
-// function toggleTheme() {
-//     const theme = document.documentElement.getAttribute('data-bs-theme');
-//     const btn = document.getElementById('theme-toggle-btn');
-
-//     if (theme === 'light') {
-//         document.documentElement.setAttribute('data-bs-theme', 'dark');
-//         btn.innerHTML = `🌙`;
-//     } else {
-//         document.documentElement.setAttribute('data-bs-theme', 'light');
-//         btn.innerHTML = `☀️`;
-//     }
-// }
-
 
 function toggleTheme() {
     const theme = document.documentElement.getAttribute('data-bs-theme');
